4d61b5861747c6da5f622f53d1668d03ee288610
jianfeng Ren <jxr041100@gmail.com>
2016-04-18 23:51:58 -0700

    now can read pos and neg images

--- object_detect_train/ReadData_pos.py
+++ object_detect_train/ReadData_pos.py
@@ -5,7 +5,7 @@ import re
 
 import Image
 from subprocess import call
-inputpath = '/home/goddess/data/car/pos'
+inputpath = '/home/jeff/data/car/pos'
 filename_prefix ='car'
 
 def get_image_size(fname):
@@ -19,13 +19,14 @@ alist_filter = ['jpg','bmp','png']
 f = open(filename_prefix+'.dat','w')
 for dirpath, dirs, files in os.walk(inputpath):	
 	for filename in files:
-		if filename[-3:] in alist_filter
+		if filename[-3:] in alist_filter:
 			fullpath = os.path.join(dirpath,filename) 
 			print "full path" +fullpath
 
 			printstr = fullpath
 			w, h = get_image_size(fullpath)
-			printstr += '\n'
+			printstr += " 1 0 0"
+			printstr += " "+str(w)+ " "+str(h)+"\n"
 			#write to the info file
 			print printstr			
 			f.write(printstr)
